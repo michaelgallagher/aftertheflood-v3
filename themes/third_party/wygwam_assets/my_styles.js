7cb69e891701ba11344399122bf503233057d122
Mike Gallagher <mail@mikegallagher.org>
2015-06-22 12:35:52 +0100

    add promo style to Wygwam + update its CSS, fix standfirst links

--- themes/third_party/wygwam_assets/my_styles.js
+++ themes/third_party/wygwam_assets/my_styles.js
@@ -1,6 +1,8 @@
 CKEDITOR.stylesSet.add( 'my_styles',
 [
     // Block Styles
-    { name: 'Note', element: 'p', attributes: { 'class': 'note' } }
+    { name: 'Note', element: 'p', attributes: { 'class': 'note' } },
+    { name: 'Promo', element: 'p', attributes: { 'class': 'promo' } }
+
 
 ]);
